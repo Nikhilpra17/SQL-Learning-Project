c34a19fb63f498263f8a588c7ea51be2d26c02b4
Nikhilpra17 <97670140+Nikhilpra17@users.noreply.github.com>
2022-11-06 23:34:43 +0530

    Add files via upload

---
+++ 1- Creating Database And Table.sql
@@ -0,0 +1,20 @@
+-- Start The CLI 
+	mqsql-ctl cli
+    
+-- Show Available DataBases 
+	SHOW DATABASES;
+    
+-- Create DataBases
+	CREATE DATABASE database_name;
+    
+-- To Delete DataBases
+	DROP DATABASE database_name;
+    
+-- Use DataBase
+	USE database_name;
+
+-- Create Table ;
+	CREATE TABLE tablename {
+    	name VARCHAR(100);
+    	age INT;
+    	}
